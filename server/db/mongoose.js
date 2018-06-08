6b19bb8fa8a77712b51b77a27c3dc240cc954741
SandhuJai <sandhujaikirat98@gmail.com>
2018-06-08 12:24:28 +0530

    Setup app for herou

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://mkbhd:hellohello123@ds147190.mlab.com:47190/todoapp_jaikirat' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose
